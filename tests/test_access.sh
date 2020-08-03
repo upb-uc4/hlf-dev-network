77dc50b3491ff44bc614020da61faa847fa83fc9
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-08-03 20:44:43 +0200

    fix pipeline 11
    Co-authored-by: Matthias Geuchen <mgeuchen@mail.uni-paderborn.de>

--- tests/test_access.sh
+++ tests/test_access.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #apply query at docker peer
-RESULT=$(docker exec -it cli peer chaincode query -n mycc -c '{"Args":["getAllCourses"]}' -C myc)
+RESULT=$(docker exec cli peer chaincode query -n mycc -c '{"Args":["getAllCourses"]}' -C myc)
 echo "$RESULT"
 if [ "$RESULT" = "\[\]" ]; then exit 1; fi
