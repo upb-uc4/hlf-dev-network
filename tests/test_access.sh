bcbf27fd84bcdd7aed17e0c9bca8a573c085c502
NikoBergemann <36935332+NikoBergemann@users.noreply.github.com>
2020-08-03 20:32:22 +0200

    Update test_access.sh
    fix pipeline 10
    
    
    Co-authored-by: Matthias Geuchen <mgeuchen@mail.uni-paderborn.de>

--- tests/test_access.sh
+++ tests/test_access.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#connect to docker
-docker exec -it cli bash
-
-RESULT=$(peer chaincode query -n mycc -c '{"Args":["getAllCourses"]}' -C myc)
+#apply query at docker peer
+RESULT=$(docker exec -it cli peer chaincode query -n mycc -c '{"Args":["getAllCourses"]}' -C myc)
 echo "$RESULT"
 if [ "$RESULT" = "\[\]" ]; then exit 1; fi
