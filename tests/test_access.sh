cf8886311385be9a97d0eb8b3229632a274d60c8
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-08-03 20:53:40 +0200

    fix pipeline 12
    Co-authored-by: Matthias Geuchen <mgeuchen@mail.uni-paderborn.de>

--- tests/test_access.sh
+++ tests/test_access.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #apply query at docker peer
-RESULT=$(docker exec cli peer chaincode query -n mycc -c '{"Args":["getAllCourses"]}' -C myc)
+RESULT=$(docker exec cli peer chaincode list --installed)
 echo "$RESULT"
-if [ "$RESULT" = "\[\]" ]; then exit 1; fi
+if [ "$RESULT" = "" ]; then exit 1; fi
