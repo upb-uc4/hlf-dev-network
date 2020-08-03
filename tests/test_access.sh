ad6e68d7da05441098cfaf1ee7847865e1010449
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-08-03 21:07:02 +0200

    pipeline test 1
    Co-authored-by: Matthias Geuchen <mgeuchen@mail.uni-paderborn.de>

--- tests/test_access.sh
+++ tests/test_access.sh
@@ -3,4 +3,5 @@
 #apply query at docker peer
 RESULT=$(docker exec cli peer chaincode list --installed)
 echo "$RESULT"
-if [[ $RESULT == "Name: myc2"* ]]; then exit 1; fi
+if [[ $RESULT == "Get installed chaincodes on peer:\nName: myc2"* ]]; then exit 0; fi
+exit 1
