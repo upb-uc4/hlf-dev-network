01dfe8baca79e59298c4dfb187364f26f636adb5
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-08-03 21:19:35 +0200

    pipeline test refactoring
    Co-authored-by: Matthias Geuchen <mgeuchen@mail.uni-paderborn.de>

--- tests/test_access.sh
+++ tests/test_access.sh
@@ -4,4 +4,10 @@
 RESULT=$(docker exec cli peer chaincode list --installed)
 echo "$RESULT"
 if [[ $RESULT =~ "Name: mycc" ]]; then exit 0; fi
+echo "#######################"
+echo "# chaincode retrieved #"
+echo "#   the wrong lists   #"
+echo "#   \"Name: mycc\"    #"
+echo "#   was not found     #"
+echo "#######################"
 exit 1
