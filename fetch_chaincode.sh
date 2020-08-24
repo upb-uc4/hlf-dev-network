dd7968c7aefc56376f53a816f5929d1dd5f62fa8
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-08-24 12:24:53 +0200

    update chaincode-repo name

--- fetch_chaincode.sh
+++ fetch_chaincode.sh
@@ -3,14 +3,14 @@
 
 #clone chaincode
 
-if [ ! -d ./hyperledger_chaincode/ ]
+if [ ! -d ./hlf-chaincode/ ]
 then
-	git clone https://github.com/upb-uc4/hyperledger_chaincode.git
+	git clone https://github.com/upb-uc4/hlf-chaincode.git
 else
 	read -p "Update existing chaincode? " -n 1 -r
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
-		pushd ./hyperledger_chaincode
+		pushd ./hlf-chaincode
 		git pull
 		popd
 	fi
