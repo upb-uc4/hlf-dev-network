a340f06a1ad4afae3727808ce2c5b4d51cc3bcca
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-11-18 12:39:08 +0100

    -c tag to unzip

--- installChaincode.sh
+++ installChaincode.sh
@@ -28,7 +28,7 @@ echo "##########################################################################
 echo "READ CHAINCODE VERSION"
 echo "############################################################################################"
 jarPath=./chaincode/UC4-chaincode/UC4-chaincode/UC4-chaincode*.jar
-unzip $jarPath META-INF/MANIFEST.MF | grep 'Implementation-Version' | cut -d ':' -f2 | tr -d ' ' | tr -d '\r' | tr -d '\n'>./chaincode/assets/testversion.txt
+unzip -c $jarPath META-INF/MANIFEST.MF | grep 'Implementation-Version' | cut -d ':' -f2 | tr -d ' ' | tr -d '\r' | tr -d '\n'>./chaincode/assets/testversion.txt
 # add access rights to file for everyone
 chmod 777 ./chaincode/assets/testversion.txt
 export CHAINCODE_VERSION=$(cat ./chaincode/assets/testversion.txt)
